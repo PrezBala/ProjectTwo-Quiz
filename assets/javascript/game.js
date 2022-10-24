c61bf3202f77a5c7497c0f7b22acb10e1ecdee74
PrezBala <prez5aw@live.com>
2022-10-24 12:42:23 +0000

    add progress bar code to java and also local storage code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,27 +20,51 @@ let questions = [
         answer: 3,
     },
     {
-        question: 'What is 2 + 2?',
-        choice1: '45',
-        choice2: '3',
-        choice3: '4',
-        choice4: 'Apple',
+        question: 'What is Ashers surname',
+        choice1: 'Taco',
+        choice2: 'Pinto',
+        choice3: 'Daniel',
+        choice4: 'Herbert',
         answer: 3,
     },
     {
-        question: 'What is 2 + 2?',
-        choice1: '45',
-        choice2: '3',
-        choice3: '4',
-        choice4: 'Apple',
-        answer: 3,
+        question: 'Capital of the UK',
+        choice1: 'Bombay',
+        choice2: 'Mumbai',
+        choice3: 'Sweden',
+        choice4: 'London',
+        answer: 4,
     },
     {
-        question: 'What is 2 + 2?',
-        choice1: '45',
-        choice2: '3',
-        choice3: '4',
-        choice4: 'Apple',
-        answer: 3,
+        question: 'When is Sabs birthday?',
+        choice1: '22nd June 1988',
+        choice2: '31st December 1912',
+        choice3: '04th July 1455',
+        choice4: '12th July 1988',
+        answer: 1,
     },
 ]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0
+    availableQuestions = {...questions}
+    getNewQuestions()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = 'Question ${questionCounter} of ${MAX_QUESTIONS}'
+    progressBarFull.style.width = '${(questioooonCounter/MAX_QUESTIONS) * 100}%'
+
+    const 
+}
