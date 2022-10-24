209c6ce013e516ea5169a197fb1f56e37fea6b4f
PrezBala <prez5aw@live.com>
2022-10-24 12:32:55 +0000

    add questions and choices, test function if working correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,44 @@ const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion ={}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions =[]
+
+let questions = [
+    {
+        question: 'What is 2 + 2?',
+        choice1: '45',
+        choice2: '3',
+        choice3: '4',
+        choice4: 'Apple',
+        answer: 3,
+    },
+    {
+        question: 'What is 2 + 2?',
+        choice1: '45',
+        choice2: '3',
+        choice3: '4',
+        choice4: 'Apple',
+        answer: 3,
+    },
+    {
+        question: 'What is 2 + 2?',
+        choice1: '45',
+        choice2: '3',
+        choice3: '4',
+        choice4: 'Apple',
+        answer: 3,
+    },
+    {
+        question: 'What is 2 + 2?',
+        choice1: '45',
+        choice2: '3',
+        choice3: '4',
+        choice4: 'Apple',
+        answer: 3,
+    },
+]
