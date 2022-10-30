1079a1097991ca3ff7fa05341d0927fefc81bd3a
PrezBala <prez5aw@live.com>
2022-10-30 13:27:45 +0000

    added MODAL overlay and also test rule images to ensure pop up opens correctly

---
+++ assets/javascript/script.js
@@ -0,0 +1,27 @@
+let openModalButtons = document.getElementById('rules-btn');
+let closeModalButtons = document.getElementById('close-btn-rules');
+let overlay = document.getElementById('overlay');
+
+openModalButtons.addEventListener('click', () => {
+    let modal = document.getElementById('modal');
+    openModal(modal);
+});
+
+closeModalButtons.addEventListener('click', () => {
+    let modal = document.getElementById('modal');
+    closeModal(modal);
+});
+
+function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+
