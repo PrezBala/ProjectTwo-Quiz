9fbbfadd830aa28e4f24a128b90b7def817e69e0
PrezBala <prez5aw@live.com>
2022-11-07 17:47:52 +0000

    amended end.js to save score, this was carried out due to the 404 error

--- assets/javascript/end.js
+++ assets/javascript/end.js
@@ -36,7 +36,7 @@ saveHighScore = e => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('html.index');
 
     
 };
