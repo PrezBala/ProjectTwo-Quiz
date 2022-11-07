67a98fa643839f99d0cf15f405e4e1fb47066db8
PrezBala <prez5aw@live.com>
2022-11-07 22:19:47 +0000

    replaced div with section id in all html

--- assets/javascript/end.js
+++ assets/javascript/end.js
@@ -1,36 +1,44 @@
-const username = document.querySelector('#username')
-const saveScoreBtn = document.querySelector('#saveScoreBtn')
-const finalScore = document.querySelector('#finalScore')
-const mostRecentScore = localStorage.getItem('mostRecentScore')
+/* set const values start*/
+const username = document.querySelector('#username');
+const saveScoreBtn = document.querySelector('#saveScoreBtn');
+const finalScore = document.querySelector('#finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-const MAX_HIGH_SCORES = 5
+const MAX_HIGH_SCORES = 5; // Set limit to Max scores
 
-finalScore.innerText = mostRecentScore
+/* set const values end*/
+
+
+/* final highscore save functionaility start*/
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value
-})
+    saveScoreBtn.disabled = !username.value;
+});
 
-saveHighScore = e => {
-    e.preventDefault()
+
+saveHighScore = e => { 
+    e.preventDefault(); 
 
     const score = {
         score: mostRecentScore,
         name: username.value
-    }
+    };
 
-    highScores.push(score)
+    highScores.push(score);
 
     highScores.sort((a,b) => {
-        return b.score - a.score
-    })
+        return b.score - a.score;
+    });
 
-    highScores.splice(5)
+    highScores.splice(5);
 
-    localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('/')
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('https://prezbala.github.io/ProjectTwo-Quiz/index.html');
 
     
-}
+};
+
+/* final highscore save functionaility start*/
